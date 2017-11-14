03c17be278c6ffb4b806d4077a28b10b0a148520
Kacper Woloszyn <kacperwoloszyn4@gmail.com>
2017-11-14 14:14:34 +0000

    added Api, Routes, and updated node modules, before changing code

---
+++ api/contacts/contacts.controller.js
@@ -0,0 +1,22 @@
+var _ = require('lodash')
+var datastore = require('../datastore');
+
+// Get all contacts
+exports.index = function(req, res) {
+    // TODO
+} ;
+
+// Creates a new contact in datastore.
+exports.create = function(req, res) {
+     // TODO
+};
+
+// Update an existing contact in datastore.
+exports.update = function(req, res) {
+      // TODO
+};
+
+// Deletes a customer from datastore.
+exports.destroy = function(req, res) {
+       // TODO
+};
---
+++ routes.js
@@ -0,0 +1,1 @@
+ app.use(‘/api/contacts’, require(’./api/contacts/index’));
