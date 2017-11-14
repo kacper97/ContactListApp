c48a2da64bc65aaa856dc7d6512596cf7f8183c2
Kacper Woloszyn <kacperwoloszyn4@gmail.com>
2017-11-14 14:48:23 +0000

    Added create new contact

--- api/contacts/contacts.controller.js
+++ api/contacts/contacts.controller.js
@@ -8,7 +8,21 @@ exports.index = function(req, res) {
 
 // Creates a new contact in datastore.
 exports.create = function(req, res) {
-     // TODO
+     var nextId = 0;
+        var last = _.last(datastore.contacts);
+        if (last != undefined) {
+           nextId = last.id + 1;
+        } else {
+          nextId = 1;
+        }
+        var contact = {
+           id: nextId,
+           name: req.body.name,
+           address: req.body.address,
+           phone_number: req.body.phone_number
+        };
+        datastore.contacts.push(contact);
+        return res.status(201).json(contact);
 };
 
 // Update an existing contact in datastore.
